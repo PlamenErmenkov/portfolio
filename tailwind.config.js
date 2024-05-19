f5210f1c36ba8d96140d1d1669dd25a058f4f13d
Fractal-Tess <vgfractal@gmail.com>
2024-05-19 17:56:38 +0300

    refactor: refactored project to react

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // glob
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
